1c1bf24acd3b1856d448de8b15e39e86d3f14dd3
santitesta <testasanty@gmail.com>
2023-04-23 13:07:32 -0300

    Bugs(Build issues - Tags and characters)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 // components/Header.js
 import React from "react";
-import { Link } from "react-scroll";
+import { Link as Link } from "react-scroll";
 
 const listItemStyle =
   "transition duration-200 ease-in-out text-white cursor-pointer active:text-blue-500 hover:bg-blue-500 py-2 px-4 rounded inline-block";
@@ -11,7 +11,15 @@ const Header = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="text-white font-bold text-xl">
-            <a href="/">Startup Logo</a>
+            <Link
+                  to="hero"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className={listItemStyle}
+                >
+                  Startup Logo
+                </Link>
           </div>
           <nav className="">
             <ul className="flex space-x-6">
